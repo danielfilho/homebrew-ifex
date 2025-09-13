050a68716b381f5405b6abfe917abba2b8c5e9a6
Daniel Filho <daniel@dnl.dev>
2025-09-13 11:17:50 +0200

    Update ifex to version 1.105.0
    - Bump version from 1.103.1 to 1.105.0
    - Update download URLs to point to v1.105.0 release
    - Update SHA256 checksums for all platform binaries

--- Formula/ifex.rb
+++ Formula/ifex.rb
@@ -1,13 +1,13 @@
 class Ifex < Formula
   desc "A modern CLI tool for managing EXIF data in JPEG, TIFF, DNG and RAW image files"
   homepage "https://github.com/danielfilho/ifex"
-  version "1.104.0"
+  version "1.105.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/danielfilho/ifex/releases/download/v1.104.0/ifex-macos-x86_64"
-      sha256 "87111d597833cd0c5e1d882fc4511b13df5fc2d25d29a4a24692a6e6c420db12"
+      url "https://github.com/danielfilho/ifex/releases/download/v1.105.0/ifex-macos-x86_64"
+      sha256 "d33796c8c9585c89ada15775d64c1d9162588f44cfaf7026f65563271ba85242"
 
       def install
         bin.install "ifex-macos-x86_64" => "ifex"
@@ -15,8 +15,8 @@ class Ifex < Formula
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/danielfilho/ifex/releases/download/v1.104.0/ifex-macos-aarch64"
-      sha256 "02d30cd043f240b63bc52cdac35b63aab6dcccc2c7f0f621d8c12ec8c59b12f6"
+      url "https://github.com/danielfilho/ifex/releases/download/v1.105.0/ifex-macos-aarch64"
+      sha256 "2b3ec07e322d149fca9df6be61171a9179cfe2e254db992b100515e4c8bc2a92"
 
       def install
         bin.install "ifex-macos-aarch64" => "ifex"
@@ -26,8 +26,8 @@ class Ifex < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/danielfilho/ifex/releases/download/v1.104.0/ifex-linux-x86_64"
-      sha256 "a461135d3d487dbfadbc88114021be0a8c2a0a0ea15d339668cbb14cf18b3ed1"
+      url "https://github.com/danielfilho/ifex/releases/download/v1.105.0/ifex-linux-x86_64"
+      sha256 "69d0075d4b67919f1de35395e3dc620a5105a9149014620ab23aa26320d7ec9f"
 
       def install
         bin.install "ifex-linux-x86_64" => "ifex"
